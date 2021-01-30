9235e2b0de7daecdcb23e2e8403a7e8a4a2a0277
younghwani <younghwankim624@gmail.com>
2021-01-30 20:12:51 +0900

    indeed 함수 정리

--- indeed.py
+++ indeed.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, 'html.parser')
     pagination = soup.find('div', class_='pagination')
@@ -30,7 +30,7 @@ def extract_job(html):
     return {'title':title, 'company':company, 'location':location,
     'link':f'https://www.indeed.com/viewjob?jk={job_id}'}
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
         print(f'Scrapping page {page}')
@@ -42,3 +42,7 @@ def extract_indeed_jobs(last_page):
             jobs.append(job)
     return jobs
 
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
+    return jobs
